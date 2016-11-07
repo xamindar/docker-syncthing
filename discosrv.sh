2beae4c2845d148bc035da3c6d66153f42823150
xamindar <xamindar@gmail.com>
2016-11-07 04:34:48 -0600

    Corrected discovery and relay commands

--- discosrv.sh
+++ discosrv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #start discosrv using environment variables
 echo -e "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n!!!!!!!!!!!!!!!! STARTING DISCOVERY SERVER !!!!!!!!!!!!!!!!\nuse instructions: https://docs.syncthing.net/users/stdiscosrv.html\nModify the following Discovery server URL accordingly and add it to all of your syncthing clients:\nhttps://[unraid machine address]/v2/?id=[Server device ID from next line]"
-/opt/discosrv/stdiscosrv -cert="$SYNCTHING_CONFIG/discosrv/cert.pem" -key="$SYNCTHING_CONFIG/discosrv/key.pem" -stats-file="$SYNCTHING_CONFIG/discosrv/stats.log"
+/opt/discosrv/stdiscosrv -cert="$SYNCTHING_CONFIG/discosrv/cert.pem" -key="$SYNCTHING_CONFIG/discosrv/key.pem" #-stats-file="$SYNCTHING_CONFIG/discosrv/stats.log"
 echo "!!!!!!!!! Discovery service died! Ensure config variables are set correctly. !!!!!!!!!!"
